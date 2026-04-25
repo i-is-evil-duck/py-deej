8554e96d11e9e86ed2cd637f574111b2c3b21f3f
I-is-evil-duck <cokie1479@gmail.com>
2026-04-24 17:53:46 -0700

    exclude platform-specific modules from build

--- build.py
+++ build.py
@@ -6,19 +6,13 @@ spec_dir = Path(__file__).parent
 
 system = platform.system()
 if system == "Windows":
-    ext = ".exe"
     app_name = "pydeej.exe"
-    data_flag = "--add-data"
     data_sep = ";"
 elif system == "Darwin":
-    ext = ".app"
     app_name = "pydeej"
-    data_flag = "--add-data"
     data_sep = ":"
 else:
-    ext = ""
     app_name = "pydeej"
-    data_flag = "--add-data"
     data_sep = ":"
 
 config_data = f"{spec_dir / 'config.yaml'}{data_sep}."
@@ -28,11 +22,17 @@ args = [
     f"--name={app_name}",
     "--onefile",
     "--console",
-    f"{data_flag}={config_data}",
-    "--hidden-import=comtypes",
+    f"--add-data={config_data}",
     f"--distpath={spec_dir}/dist",
     f"--workpath={spec_dir}/build",
     f"--specpath={spec_dir}",
 ]
+
+if system == "Windows":
+    args.append("--hidden-import=comtypes")
+    args.append("--exclude-module=pulsectl")
+elif system == "Linux":
+    args.append("--exclude-module=pycaw")
+    args.append("--exclude-module=comtypes")
 
 PyInstaller.__main__.run(args)
